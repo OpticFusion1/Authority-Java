501c2780dad5b678bc5ffbecc6434417c203ac03
Algore <teamxskill@gmail.com>
2017-03-30 11:23:00 -0700

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -4,6 +4,7 @@ import java.util.Random
 public class mainClass {
 public static main(String[], arg) {
   Scanner dec = new Scanner(System.out);
+  Scanner wait = new Scanner(System.out);
   Random encounter = new Random();
   game gameOBject = new game();
   
