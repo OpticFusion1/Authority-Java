f57b2eebd9c4300ee363d1205a6e34e9ae0be3ae
Algore <teamxskill@gmail.com>
2017-03-30 11:24:11 -0700

    Update game.java

--- game.java
+++ game.java
@@ -1,1 +1,17 @@
-
+public class game() {
+  private String name;
+  private int hp;
+  private int rank;
+  private int weapon;
+  private int level;
+  int rest;
+  public void newg() {
+  System.out.println("What is your name");
+    name = dec.newString();
+   System.out.println("Welcome cadet "+ name);
+    System.out.println("There will be pauses like this you can press enter to continue");
+    rest = wait.newString();
+    System.out.println("");   
+    
+  }
+}
