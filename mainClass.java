4ade170064060b6a587c9955eea82e42002c95fb
Algore <teamxskill@gmail.com>
2017-03-30 10:59:51 -0700

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -5,13 +5,14 @@ public class mainClass {
 public static main(String[], arg) {
   Scanner dec = new Scanner(System.out);
   Random encounter = new Random();
+  game gameOBject = new game();
   
   System.out.print("Menu\n1. new game\n2. continue\n3. quit");
   decision = dec.netInt();
   switch(decision) {
-  case 1: newg();break;
-  case 2: cont();break;
-  case 3: quit();break;
+  case 1: gameObject.newg();break;
+  case 2: gameObject.cont();break;
+  case 3: gameObject.quit();break;
   default: main();
 }
 }
